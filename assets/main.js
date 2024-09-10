10e0dd66ebbd51384181783e10211120b9b6db0d
gatino <gatinocorp@gmail.com>
2024-09-10 09:55:16 -0300

    ...

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,27 @@
-const btsToggleNav = document.querySelectorAll('button.toggle-nav')
-const nv = document.querySelector('nav')
+const domBtsToggleNav = document.querySelectorAll('button.toggle-nav')
+const domNav = document.querySelector('nav')
 
-btsToggleNav.forEach(function(bt){
-  bt.addEventListener('click', function(){
-    nv.classList.toggle('open')
+domBtsToggleNav.forEach(function (bt) {
+  bt.addEventListener('click', function () {
+    domNav.classList.toggle('open')
   })
 })
+
+// ---------------------
+
+async function listarPersonagens() {
+  const template = document.querySelector('#modelo-personagem')
+  const request = await fetch('personagens.json')
+  const dados = await request.json()
+
+  dados.forEach(personagem => {
+    const novoDomPersonagem = template.content.cloneNode(true)
+    novoDomPersonagem.querySelector('h2').textContent = personagem.nome
+    novoDomPersonagem.querySelector('p').textContent = personagem.classe
+    template.parentNode.appendChild(novoDomPersonagem)
+  })
+}
+
+listarPersonagens()
+
+// https://rickandmortyapi.com/api/character
